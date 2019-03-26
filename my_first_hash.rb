43ec38f815af35340e4d27357524d8bcb15f8c98
Susan Tan <ssusantan9@gmail.com>
2019-03-26 03:52:00 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,9 +1,6 @@
 def my_hash
   pets = { "dog" => "pluto", "cat" => "bemo"}
-end
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
 end
 
 
