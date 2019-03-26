734acfba742bcf787d1fb9fd65ce98f2d9554e93
Susan Tan <ssusantan9@gmail.com>
2019-03-26 03:56:31 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -32,7 +32,7 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-  shipping_manifest["muskets"] = atan2
+  shipping_manifest["muskets"] = 2
 
 
 
